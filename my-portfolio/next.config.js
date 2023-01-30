7559c6304e843f4e74ce2456ee5fd4074486689c
TLRKiliann <philogenie@protonmail.com>
2023-01-30 16:05:40 +0100

    configuration for export

--- my-portfolio/next.config.js
+++ my-portfolio/next.config.js
@@ -3,9 +3,12 @@ const path = require('path')
 
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
-  },
-}
+  }
+};
 
 module.exports = nextConfig
