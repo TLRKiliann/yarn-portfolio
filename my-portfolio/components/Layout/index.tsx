3411393517bdde68475ab0c0d709834c6841c780
TLRKiliann <philogenie@protonmail.com>
2023-02-03 10:39:43 +0100

    framer-motion animation page improved

--- my-portfolio/components/Layout/index.tsx
+++ my-portfolio/components/Layout/index.tsx
@@ -2,14 +2,13 @@ import { motion } from "framer-motion";
 
 const Layout = ({ children }: any) => (
   <motion.div
-    initial={{ x: 600, width: "100%", height: "100%", opacity: 0, backgroundColor: "#193dbb" }}
-    animate={{ x: 0, width: "100%", height: "100%", opacity: 1, backgroundColor: "#193dbb" }}
-    exit={{ x: -600, width: "100%", height: "100%", opacity: 0, backgroundColor: "#193dbb" }}
+    initial={{ x: 600, opacity: 0, backgroundColor: "#193dbb" }}
+    animate={{ x: 0, opacity: 1, backgroundColor: "#193dbb" }}
+    exit={{ x: -600, opacity: 0 }}
     transition={{
-      type: "tween",
       stiffness: 600,
       damping: 28,
-      ease: "easeOut",
+      ease: [0,0,0.5,1],
     }}
   >
     {children}
