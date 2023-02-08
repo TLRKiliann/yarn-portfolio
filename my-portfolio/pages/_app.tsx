b725fea778ebe8c2041e85644b1b27e96545cc42
TLRKiliann <philogenie@protonmail.com>
2023-02-08 23:48:41 +0100

    framer-motion at work

--- my-portfolio/pages/_app.tsx
+++ my-portfolio/pages/_app.tsx
@@ -1,15 +1,17 @@
 import "@fontsource/acme"
 import "@fontsource/akaya-kanadaka"
+import "@fontsource/abhaya-libre"
+import { AnimatePresence, motion } from 'framer-motion'
 import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
-import { motion, AnimatePresence } from "framer-motion"
-import { useRouter } from "next/router"
 
-export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter()
+export default function App({ Component, pageProps, router }: AppProps) {
   return (
-    <AnimatePresence initial={false} exitBeforeEnter>
-        <Component {...pageProps} key={router} />
+    <AnimatePresence mode="wait" initial={false}>
+      <motion.div>
+        <Component {...pageProps} key={router.asPath}/>
+      </motion.div>
     </AnimatePresence>
   )
 }
+
