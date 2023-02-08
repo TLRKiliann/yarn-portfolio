592d319f78c1f442759ed6756392be89653c2d8b
TLRKiliann <philogenie@protonmail.com>
2023-02-08 23:48:18 +0100

    framer-motion at work

--- my-portfolio/components/Layout/index.tsx
+++ my-portfolio/components/Layout/index.tsx
@@ -1,14 +1,14 @@
 import { motion } from "framer-motion";
 
-const Layout = ({ children, router }: any) => (
+const Layout = ({ children }:any) => (
   <motion.div
-    key={router}
-    initial={{ opacity: 0.5, y: 200, scale: 0 }}
-    animate={{ opacity: 1, y: 0, scale: 1 }}
-    exit={{ opacity: 0.5, y: 200, scale: 0 }}
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    exit={{ opacity: 0 }}
     transition={{
-      duration: 0.8,
-      ease: "easeOut",
+      type: "spring",
+      stiffness: 260,
+      damping: 20,
     }}
   >
     {children}
