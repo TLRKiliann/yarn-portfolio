bbd3c97544a876c0a9b4672f0b78519341f1499b
TLRKiliann <philogenie@protonmail.com>
2023-02-11 13:08:07 +0100

    code refactor & revert animation added

--- my-portfolio/pages/about/index.tsx
+++ my-portfolio/pages/about/index.tsx
@@ -1,47 +1,43 @@
 import React, { useState } from 'react'
 import Layout from "../../components/Layout"
 import ButtonGoHome from "../../components/ButtonGoHome"
+import AboutFirstParagraph from "../../components/AboutComponent/AboutFirstParagraph"
+import AboutSecondParagraph from "../../components/AboutComponent/AboutSecondParagraph"
+import AboutThirdParagraph from "../../components/AboutComponent/AboutThirdParagraph"
+import AboutFourthParagraph from "../../components/AboutComponent/AboutFourthParagraph"
+import AboutFifthParagraph from "../../components/AboutComponent/AboutFifthParagraph"
+import BtnArrow from "../../components/AboutComponent/BtnArrow"
 import Link from 'next/link'
 import {BsFillArrowRightCircleFill} from 'react-icons/bs'
 import Image from 'next/image'
 import styles from '@/styles/About.module.scss'
 
-type transitionTxtProps = {
-  first: boolean
-  second: boolean
-  third: boolean
-  four: boolean
-  five: boolean
-}
-
 const About:React.FC = () => {
 
-  const [transitionTxt, setTransitionTxt] = useState<transitionTxtProps>({
-    first: false,
-    second: false,
-    third: false,
-    four: false,
-    five: false
-  })
-
+  const [first, setFirst] = useState<boolean>(false)
+  const [second, setSecond] = useState<boolean>(false)
+  const [third, setThird] = useState<boolean>(false)
+  const [four, setFour] = useState<boolean>(false)
+  const [five, setFive] = useState<boolean>(false)
+  
   const handleFirst = () => {
-    setTransitionTxt({...transitionTxt, first: !transitionTxt.first})
+    setFirst(first => !first)
   }
 
   const handleSecond = () => {
-    setTransitionTxt({...transitionTxt, second: !transitionTxt.second})
+    setSecond(second => !second)
   }
 
   const handleThird = () => {
-    setTransitionTxt({...transitionTxt, third: !transitionTxt.third})
+    setThird(third => !third)
   }
 
   const handleFour = () => {
-    setTransitionTxt({...transitionTxt, four: !transitionTxt.four})
+    setFour(four => !four)
   }
 
   const handleFive = () => {
-    setTransitionTxt({...transitionTxt, five: !transitionTxt.five})
+    setFive(five => !five)
   }
 
   return (
@@ -66,36 +62,23 @@ const About:React.FC = () => {
           
           <div className={styles.listabout}>
 
-            {transitionTxt.first === false ? (
-              <button type='button' onClick={handleFirst} className={styles.button}>
-                <BsFillArrowRightCircleFill className={styles.logoreact} size={24}/>
-              </button>
+            {first === false ? (
+              <BtnArrow boolChange={first} handleChangeFunc={handleFirst} />
               ) : (
-              <button type='button' onClick={handleFirst} className={styles.button}>
-                <BsFillArrowRightCircleFill className={styles.rotationlogo} size={24}/>
-              </button>
+              <BtnArrow boolChange={first} handleChangeFunc={handleFirst} />
               )
             }
+
             <a className={styles.aboutclassa}>
               2017-2018
             </a>
           </div>
 
-          {transitionTxt.first === true ? (
-            <div className={styles.paragraphaboutfirst}>
-              <li className={styles.li}>
-                <Link className={styles.link} href="https://github.com/TLRKiliann/server-PHP-MySQL">
-                  Premier site en HTML, CSS, PhP, MySQL avec server Apache2 
-                  raspberry (WAN) HTTPS
-                </Link>
-              </li>
-              <li className={styles.li}>
-                <Link className={styles.link} href="https://github.com/TLRKiliann/apache2_conf_raspberry">
-                  Configuration https apache2-ssl
-                </Link>
-              </li>
-            </div>
-            ) : null
+          {first === true ? (
+            <AboutFirstParagraph boolStyles={first}/>
+            ) : (
+            <AboutFirstParagraph boolStyles={first}/>
+            )
           }
 
           <hr className={styles.hrabout}/>
@@ -103,14 +86,10 @@ const About:React.FC = () => {
 
           <div className={styles.listabout}>
 
-            {transitionTxt.second === false ? (
-              <button type='button' onClick={handleSecond} className={styles.button}>
-                <BsFillArrowRightCircleFill className={styles.logoreact} size={24}/>
-              </button>
+            {second === false ? (
+              <BtnArrow boolChange={second} handleChangeFunc={handleSecond} />
               ) : (
-              <button type='button' onClick={handleSecond} className={styles.button}>
-                <BsFillArrowRightCircleFill className={styles.rotationlogo} size={24}/>
-              </button>
+              <BtnArrow boolChange={second} handleChangeFunc={handleSecond} />
               )
             }
 
@@ -119,30 +98,11 @@ const About:React.FC = () => {
             </a>
           </div>
 
-          {transitionTxt.second === true ? (
-            <div className={styles.paragraphaboutfirst}>
-              <li className={styles.li}>
-                <Link className={styles.link} href="https://github.com/TLRKiliann/BMI">
-                  BMI (python3-tkinter)
-                </Link>
-              </li>
-              <li className={styles.li}>
-                <Link className={styles.link} href="https://github.com/TLRKiliann/phpython-server">
-                  phpython-server
-                </Link>
-              </li>
-              <li className={styles.li}>
-                <Link className={styles.link} href="https://www.youtube.com/watch?v=O70eptxwP3I">
-                  ICMP transmission between 3 VM (one router)
-                </Link>
-              </li>
-              <li className={styles.li}>
-                <Link className={styles.link} href="https://www.youtube.com/watch?v=zaWTRN0kmYw">
-                  Convert a VM to a router (Debian 10)
-                </Link>
-              </li>
-            </div>
-            ) : null
+          {second === true ? (
+            <AboutSecondParagraph boolStyles={second} />
+            ) : (
+            <AboutSecondParagraph boolStyles={second} />
+            )
           }
 
           <hr className={styles.hrabout}/>
@@ -150,14 +110,10 @@ const About:React.FC = () => {
           
           <div className={styles.listabout}>
 
-            {transitionTxt.third === false ? (
-              <button type='button' onClick={handleThird} className={styles.button}>
-                <BsFillArrowRightCircleFill className={styles.logoreact} size={24}/>
-              </button>
+            {third === false ? (
+              <BtnArrow boolChange={third} handleChangeFunc={handleThird} />
               ) : (
-              <button type='button' onClick={handleThird} className={styles.button}>
-                <BsFillArrowRightCircleFill className={styles.rotationlogo} size={24}/>
-              </button>
+              <BtnArrow boolChange={third} handleChangeFunc={handleThird} />
               )
             }
 
@@ -166,51 +122,23 @@ const About:React.FC = () => {
             </a>
           </div>
 
-          {transitionTxt.third === true ? (
-            <div className={styles.paragraphaboutfirst}>
-              <li className={styles.li}>
-                <Link className={styles.link} href="https://github.com/TLRKiliann/tor-proxy">
-                  tor proxy on linux and raspberry pi 400
-                </Link>
-              </li>
-              <li className={styles.li}>
-                <Link className={styles.link} href="https://github.com/TLRKiliann/anxious_scale">
-                  anxious_scale (stand-alone) échelle d'évaluation des angoisse (psychatrie)
-                </Link>
-              </li>
-              <li className={styles.li}>
-                <Link className={styles.link} href="https://github.com/TLRKiliann/psychotabs">
-                  psychotabs (stand-alone)
-                </Link>
-              </li>
-              <li className={styles.li}>
-                <Link className={styles.link} href="https://github.com/TLRKiliann/interactions">
-                  interactions python3-tkinter (stand-alone)
-                </Link>
-              </li>
-              <li className={styles.li}>
-                <Link className={styles.link} href="https://github.com/TLRKiliann/timetrack">
-                  timetrack (application soins infirmiers) python3-tkinter ssh
-                  scp mysql
-                </Link>
-              </li>
-            </div>
-            ) : null
+          {third === true ? (
+            <AboutThirdParagraph boolStyles={third} />
+            ) : (
+            <AboutThirdParagraph boolStyles={third} />
+            )
           }
+
 
           <hr className={styles.hrabout}/>
           <br />
           
           <div className={styles.listabout}>
 
-            {transitionTxt.four === false ? (
-              <button type='button' onClick={handleFour} className={styles.button}>
-                <BsFillArrowRightCircleFill className={styles.logoreact} size={24}/>
-              </button>
+            {four === false ? (
+              <BtnArrow boolChange={four} handleChangeFunc={handleFour} />
               ) : (
-              <button type='button' onClick={handleFour} className={styles.button}>
-                <BsFillArrowRightCircleFill className={styles.rotationlogo} size={24}/>
-              </button>
+              <BtnArrow boolChange={four} handleChangeFunc={handleFour} />
               )
             }
 
@@ -219,45 +147,11 @@ const About:React.FC = () => {
             </a>
           </div>
 
-          {transitionTxt.four === true ? (
-            <div className={styles.paragraphaboutfirst}>
-              <li className={styles.li}>
-                <Link className={styles.link} href="https://github.com/TLRKiliann/Contacts-React">
-                  Phone contact with React (Contacts-React)
-                </Link>
-              </li>
-              <li className={styles.li}>
-                <Link className={styles.link} href="https://github.com/TLRKiliann/typescript-e-commerce">
-                  site e-commerce avec typescript
-                </Link>
-              </li>
-              <li className={styles.li}>
-                <Link className={styles.link} href="https://github.com/TLRKiliann/mernstack-api">
-                  mernstack-api (vite-react-typescript-mysql)
-                </Link>
-              </li>
-              <li className={styles.li}>
-                <Link className={styles.link} href="https://www.youtube.com/watch?v=8g789tQ2gyY">
-                  site de boulangerie
-                </Link>
-              </li>
-              <li className={styles.li}>
-                <Link className={styles.link} href="https://www.youtube.com/watch?v=ggG2bOggCZU">
-                  Vite-React-TS-Tailwind & Sass
-                </Link>
-              </li>
-              <li className={styles.li}>
-                <Link className={styles.link} href="https://www.youtube.com/watch?v=sdoP_y_PftQ">
-                  Advanced chat-app (vite react ts scss mariadb) in LAN
-                </Link>
-              </li>
-              <li className={styles.li}>
-                <Link className={styles.link} href="https://github.com/TLRKiliann/nextjs-api">
-                  Next.js api
-                </Link>
-              </li>
-            </div>
-            ) : null
+          {four === true ? (
+            <AboutFourthParagraph boolStyles={four} />
+            ) : (
+            <AboutFourthParagraph boolStyles={four} />
+            )
           }
 
           <hr className={styles.hrabout}/>
@@ -265,14 +159,10 @@ const About:React.FC = () => {
           
           <div className={styles.listabout}>
 
-            {transitionTxt.five === false ? (
-              <button type='button' onClick={handleFive} className={styles.button}>
-                <BsFillArrowRightCircleFill className={styles.logoreact} size={24}/>
-              </button>
+            {five === false ? (
+              <BtnArrow boolChange={five} handleChangeFunc={handleFive} />
               ) : (
-              <button type='button' onClick={handleFive} className={styles.button}>
-                <BsFillArrowRightCircleFill className={styles.rotationlogo} size={24}/>
-              </button>
+              <BtnArrow boolChange={five} handleChangeFunc={handleFive} />
               )
             }
 
@@ -281,13 +171,11 @@ const About:React.FC = () => {
             </a>
           </div>
           
-          {transitionTxt.five === true ? (
-            <div className={styles.paragraphaboutfirst}>
-              Actuellement en reconversion professionnelle, je souhaite faire de ma passion
-              un métier et continuer de faire évoluer mes compétences avec des pros au sein
-              d'une entreprise.
-            </div>
-            ) : null
+          {five === true ? (
+            <AboutFifthParagraph boolStyles={five} />
+            ) : (
+            <AboutFifthParagraph boolStyles={five} />
+            )
           }
 
           <hr className={styles.hrabout} style={{marginBottom: "0px"}} />
