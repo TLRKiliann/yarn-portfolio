4133107606fd4ed0f03bedf96c110f477eef4722
TLRKiliann <philogenie@protonmail.com>
2023-02-17 17:54:36 +0100

    style improved

--- my-portfolio/pages/evolution/index.tsx
+++ my-portfolio/pages/evolution/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
-//import Image from 'next/image'
+import Image from 'next/image'
 import Layout from "../../components/Layout"
 import ButtonGoHome from "../../components/ButtonGoHome"
 import styles from '@/styles/Evolution.module.scss'
@@ -22,14 +22,27 @@ const Evolution:React.FC = () => {
 
         <ButtonGoHome />
 
+        <div className={styles.mainclassvideo}>
+          <video 
+            width="100%"
+            height="100%"
+            autoPlay 
+            muted 
+            loop 
+            className={styles.divvideointro}>
+            <source src={"./videos/abstract.mp4"} type="video/mp4"/>
+          </video>
+        </div>
+
         <div className={styles.articlecontainer}>
+
           <div className={styles.article}>
             <h2 className={styles.h2}>Story</h2>
             <p className={styles.p}>
-              Mon premier OS était un mac book G3. Seulement, l'information n'était pas aussi
-              accessible qu'aujourd'hui. Je me souviens encore des pages internet sans mise
-              en forme ni couleurs. J'étais très frustré de ne pas comprendre comment fonctionnait
-              mon ordinateur de l'époque.
+              Mon premier ordinateur était un mac book G3. Seulement, l'information
+              n'était pas aussi accessible qu'aujourd'hui. Je me souviens encore des
+              pages internet sans mise en forme ni couleurs. J'étais très frustré de
+              ne pas comprendre comment fonctionnait mon ordinateur de l'époque.
             </p>
           </div>
           <div className={styles.article2}>
@@ -43,23 +56,37 @@ const Evolution:React.FC = () => {
               communcations internet.
             </p>
           </div>
+        </div>
+
+        <div className={styles.articlecontainer}>
           <div className={styles.article3}>
             <h2 className={styles.h2}>Personnalité</h2>
             <p className={styles.p}>
-              Je suis quelqu'un de rigoureux, structuré et curieux. J'aime me tenir informé
-              sur les nouvelles technologies, celles qui sont les meilleures ainsi que sur
-              les bonnes pratiques.
+              Je suis quelqu'un de rigoureux, structuré et curieux. J'aime me tenir 
+              informé sur les nouvelles technologies, celles qui sont les meilleures 
+              ainsi que sur les bonnes pratiques.
             </p>
             <p className={styles.p}>
-              La sécurité occupe également une place importante, surtout au jour d'aujourd'hui.
+              La sécurité occupe également une place importante, surtout au jour
+              d'aujourd'hui.
             </p>
           </div>
           
           <div className={styles.article4}>
             
+            <div className={styles.imgevolution}>
+              <Image
+                src={'/images/super.png'}
+                width="1920"
+                height="1280"
+                alt="img about"
+                className={styles.img}
+              />
+            </div>
+
             <h2 className={styles.h2}>Skills</h2>
-            <p className={styles.p}>
-              Cliquer les liens !
+            <p style={{fontSize: "1.4rem", color: 'dodgerblue'}}>
+              Click !
             </p>
             <div className={styles.lilink}>
               <li className={styles.li}>
@@ -75,27 +102,29 @@ const Evolution:React.FC = () => {
             </div>
           
           </div>
-          
+        </div>
+
+        <div className={styles.articlecontainer}>
           <div className={styles.article5}>
             <h2 className={styles.h2}>Objectifs</h2>
             <p className={styles.p}>
-              Devenir un excellent développeur et travailler avec des pros serait pour moi l'idéal.
-              Plusieurs cerveaux valent toujours mieux qu'un ! Aussi, je pense que pour construire et
-              s'enrichir 
+              Devenir un excellent développeur et travailler avec des pros serait
+              pour moi l'idéal. Plusieurs cerveaux valent toujours mieux qu'un ! 
+              Aussi, je pense que pour construire et s'enrichir il faut être plusieurs.
             </p>
           </div>
           <div className={styles.article6}>
             <h2 className={styles.h2}>Valeurs</h2>
             <p className={styles.p}>
-              Identifier l'origine du problème et faire des liens pour trouver des solutions,
-              est une valeur importante que l'on retrouve aussi dans les soins. Je pense que
-              c'est une valeur importante également en informatique. 
-
+              Identifier l'origine du problème et faire des liens pour trouver des 
+              solutions, est une valeur importante que l'on retrouve aussi dans les 
+              soins. Je pense que c'est une valeur importante également en informatique. 
+            </p>
+            <p className={styles.p}>
               J'aime trouver des solutions et paramétrer les situations. Réfléchir 
               avant, pendant et après l'action. La remise en question et les remarques 
-              sont pour moi essentiel pour s'amélioration et évoluer.
+              sont pour moi essentiels pour s'amélioration et évoluer.
               J'aime apprendre découvrir des nouvelles choses.
-
               J'aime créer, innover et contribuer.
             </p>
           </div>
